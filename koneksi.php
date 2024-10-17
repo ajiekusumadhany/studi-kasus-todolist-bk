684a2727e915c0fe92c4614036957c91c74bb0aa
ajiekusumadhany <ajieikruskusumadhany@gmail.com>
2024-10-18 03:07:32 +0700

    first commit

---
+++ koneksi.php
@@ -0,0 +1,11 @@
+<?php
+$databaseHost = 'localhost';
+$databaseName = 'kegiatan';
+$databaseUsername = 'root';
+$databasePassword = '';
+
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
+
+if (!$mysqli) {
+    die("Koneksi gagal: " . mysqli_connect_error());
+}
